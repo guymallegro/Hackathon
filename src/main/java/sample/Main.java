1acaada813e3e0a527132794327d3fbe6baa8300
Guy Marushchenko <guym@allegro.ai>
2019-03-28 21:42:37 +0200

    Add server for updating clients

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,10 +2,7 @@ package sample;
 
 import com.darkprograms.speech.microphone.Microphone;
 import com.darkprograms.speech.recognizer.GSpeechDuplex;
-import com.darkprograms.speech.recognizer.GSpeechResponseListener;
-import com.darkprograms.speech.recognizer.GoogleResponse;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
@@ -19,31 +16,27 @@ import javafx.stage.Modality;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import net.sourceforge.javaflacencoder.FLACFileWriter;
 
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-
+import java.util.*;
+import java.text.DecimalFormat;
+import static sun.awt.FontConfiguration.verbose;
+import java.net.ServerSocket;
 
 public class Main extends Application {
     public final static String GOOGLE_KEY = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw";
-    public TextArea response;
+    private final static int PORT = 1978;
     public ImageView listening;
-    public ImageView taptospeak;
-    public Button stopbutton;
-    private GSpeechDuplex duplex;
+    HashMap<String, Integer> wordsFreq;
+    static HashMap<String, Double> wordDistribution;
     public ImageView play;
     public ImageView pause;
     public ImageView stop;
-    private Microphone mic;
-    HashMap<String,Integer> wordsFreq;
-    static HashMap<String,Double> wordDistribution;
     public ComboBox lang;
-    public Button buttonstatistic;
     Stage stage;
 
     @Override
@@ -63,12 +56,34 @@ public class Main extends Application {
         launch(args);
     }
 
-    public void initialize (){
+    public void initialize() {
         lang.getItems().add("English");
         lang.getItems().add("Hebrew");
         lang.getItems().add("Arabic");
+        new Thread(this::startServer).start();
+    }
 
+    private void startServer() {
+        try {
+            ServerSocket serverConnect = new ServerSocket(PORT);
+            System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
 
+            // we listen until user halts server execution
+            while (true) {
+                JavaHTTPServer myServer = new JavaHTTPServer(serverConnect.accept());
+
+                if (verbose) {
+                    System.out.println("Connecton opened. (" + new Date() + ")");
+                }
+
+                // create dedicated thread to manage the client connection
+                Thread thread = new Thread(myServer);
+                thread.start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Server Connection error : " + e.getMessage());
+        }
     }
 
 
@@ -83,28 +98,27 @@ public class Main extends Application {
             stage.setX(primaryScreenBounds.getMinX());
             stage.setY(primaryScreenBounds.getMinY());
             stage.setWidth(primaryScreenBounds.getWidth());
-            stage.setHeight(primaryScreenBounds.getHeight()/10);
+            stage.setHeight(primaryScreenBounds.getHeight() / 10);
             play.setVisible(false);
             listening.setVisible(true);
             stage.setTitle("My New Stage Title");
             stage.setScene(new Scene(subtitlesWindow, 450, 450));
             stage.show();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void Statistics(){
+    public void Statistics() {
 
-        wordsFreq=ViewSubtitles.getDictionary();
-        wordDistribution=new HashMap<String,Double> ();
-        int NumOfWords=0;
+        wordsFreq = ViewSubtitles.getDictionary();
+        wordDistribution = new HashMap<String, Double>();
+        int NumOfWords = 0;
 
-        for (String word:
+        for (String word :
                 wordsFreq.keySet()) {
 
-            NumOfWords=NumOfWords+wordsFreq.get(word);
+            NumOfWords = NumOfWords + wordsFreq.get(word);
         }
 
         List<String> keys = new ArrayList(wordsFreq.keySet());
@@ -117,8 +131,6 @@ public class Main extends Application {
 
             wordDistribution.put(keys.get(i),new Double(x));
         }
-
-
         try {
             Stage startWindow;
             startWindow = new Stage();
@@ -130,19 +142,19 @@ public class Main extends Application {
             Scene scene2 = new Scene(root2, 500, 650);
             startWindow.setScene(scene2);
             startWindow.show();
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static HashMap<String,Double> getDistribution(){return wordDistribution; };
+    public static HashMap<String, Double> getDistribution() {
+        return wordDistribution;
+    }
 
     public void Stop() {
         stage.close();
-        listening.setVisible(false);
         play.setVisible(true);
-        //buttonstatistic.setDisable(false);
-//        duplex.stopSpeechRecognition();
+        listening.setVisible(false);
+        play.setDisable(false);
     }
 }
--- src/main/java/sample/ViewSubtitles.java
+++ src/main/java/sample/ViewSubtitles.java
@@ -4,20 +4,20 @@ import com.darkprograms.speech.microphone.Microphone;
 import com.darkprograms.speech.recognizer.GSpeechDuplex;
 import com.darkprograms.speech.recognizer.GSpeechResponseListener;
 import com.darkprograms.speech.recognizer.GoogleResponse;
-import com.sun.org.apache.xml.internal.resolver.readers.ExtendedXMLCatalogReader;
 import javafx.scene.control.TextArea;
 import net.sourceforge.javaflacencoder.FLACFileWriter;
 
+import java.io.*;
 import java.util.HashMap;
 
 public class ViewSubtitles {
     public TextArea shownText;
     private GSpeechDuplex duplex;
     private Microphone mic;
-    static HashMap<String,Integer> wordsFreq;
+    static HashMap<String, Integer> wordsFreq;
 
-    public void initialize(){
-        wordsFreq=new HashMap<String,Integer>();
+    public void initialize() {
+        wordsFreq = new HashMap<String, Integer>();
         shownText.setStyle("-fx-font-size: 50 Thaoma; -fx-font-weight: bold");
         mic = new Microphone(FLACFileWriter.FLAC);
         duplex = new GSpeechDuplex(Main.GOOGLE_KEY);
@@ -30,21 +30,20 @@ public class ViewSubtitles {
                     if (gr.getResponse() == null) {
                         return;
                     }
-
                     shownText.setText(output);
-                    String [] words = output.split(" ");
+                    addToHtml(shownText.getText());
+                    String[] words = output.split(" ");
                     for (String w :
-                         words) {
-                        if(wordsFreq.containsKey(w)){
+                            words) {
+                        if (wordsFreq.containsKey(w)) {
                             int num = wordsFreq.get(w);
                             wordsFreq.remove(w);
-                            wordsFreq.put(w,num+1);
-                        }
-                        else
-                            wordsFreq.put(w,1);
+                            wordsFreq.put(w, num + 1);
+                        } else
+                            wordsFreq.put(w, 1);
                     }
 
-                }catch (Exception e){
+                } catch (Exception e) {
 
                 }
             }
@@ -58,8 +57,28 @@ public class ViewSubtitles {
         }).start();
     }
 
-public static HashMap<String,Integer> getDictionary(){
+    public static HashMap<String, Integer> getDictionary() {
         return wordsFreq;
-}
+    }
+
+    private void addToHtml(String text) throws IOException {
+//        String html = "<!DOCTYPE html>\n" +
+//                "<html lang=\"he\">\n" +
+//                "<head>\n" +
+//                "    <meta charset=\"utf-8\" />\n" +
+//                "    <meta http-equiv=\"refresh\" content=\"0.1\"/>\n" +
+//                "    <title>דף בעברית</title>\n" +
+//                "</head>\n" +
+//                "\n" +
+//                "<body>\n" +
+//                "\n" +
+//                "<p>";
+        PrintWriter pw = new PrintWriter("index.html");
+        pw.close();
+        OutputStream os = new FileOutputStream(new File("index.html"), true);
+        os.write((text).getBytes(), 0, ((text)).length());
+        os.write(("</p></body>").getBytes(), 0, ("</p></body>").length());
+        os.close();
+    }
 
 }
